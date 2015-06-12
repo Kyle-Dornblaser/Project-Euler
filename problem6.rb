77e2c341c0ab4565dab83f8409b138872ffbe9f9
Kyle Dornblaser <kdornblaser@gmail.com>
2015-06-12 14:43:39 -0400

    Add solution for problem 6.

---
+++ problem6.rb
@@ -0,0 +1,9 @@
+def sum_square_difference(n)
+  sum_of_squares = (n ** 3.0 / 3.0) + (n ** 2.0 / 2.0) + (n / 6.0)
+  square_of_sum = ((n * (n + 1.0)) / 2.0) ** 2.0
+  difference = square_of_sum - sum_of_squares
+  difference === Float::INFINITY ? difference : difference.to_i
+end
+
+max = ARGV.first ? ARGV.first.to_i : 10
+puts sum_square_difference(max)
