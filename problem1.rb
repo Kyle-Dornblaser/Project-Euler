d5befea172d400ac36272a34a327bf80d879ad50
Kyle Dornblaser <kdornblaser@gmail.com>
2015-06-11 22:58:13 -0400

    Solve problem one.

---
+++ problem1.rb
@@ -0,0 +1,11 @@
+
+def sum_of_multiples_below(n, m1 = 3, m2 = 5)
+  sum = 0
+  for i in 1...n do
+    sum += i if i % m1 === 0 || i % m2 === 0
+  end
+  sum
+end
+
+max_plus_one = ARGV.first ? ARGV.first.to_i : 10
+puts sum_of_multiples_below(max_plus_one)
