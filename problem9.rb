5d1293c23d6957c4ddc2ef7cd536722a7a849ab1
Kyle Dornblaser <kdornblaser@gmail.com>
2015-06-12 15:04:33 -0400

    Add solution for problem 9.

---
+++ problem9.rb
@@ -0,0 +1,21 @@
+def special_pythagorean_triplet
+  special_sum = 1000
+  # need to leave room for at least 1 + 1 + (special_sum - 2)
+  combinations = []
+  for i in 1...special_sum -1 do
+    for j in 1...special_sum -1 do
+      combinations << [i, j, special_sum - i - j].sort if special_sum - i - j > 0
+    end
+  end
+  unique_combinations = combinations.uniq
+  triplet_index = -1
+  unique_combinations.each.with_index do |combo, index|
+    if combo[0] ** 2 + combo[1] ** 2 === combo[2] ** 2
+      triplet_index = index
+    end
+  end
+  unique_combinations[triplet_index]
+end
+
+# Product of triplet
+puts special_pythagorean_triplet.reduce(:*)
