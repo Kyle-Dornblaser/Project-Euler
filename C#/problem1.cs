dbe768385088c1abbc75aaa37bbee089923173e2
Kyle Dornblaser <kdornblaser@gmail.com>
2015-06-23 13:38:24 -0400

    Update problem1.cs to handle larger numbers.

--- C#/problem1.cs
+++ C#/problem1.cs
@@ -1,13 +1,27 @@
 using System;
+using System.Diagnostics;
 
 class Problem1
 {
-  public static void Main()
+  public static void Main(string[] args)
   {
-    int n = 1000;
-    int m1 = 3;
-    int m2 = 5;
-    int sum = 0;
+    long max_plus_one = 10;
+    if (args.Length > 0)
+    {
+      max_plus_one = Convert.ToInt64(args[0]);
+    }
+
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
+    long sum = SumOfMultiplesBelow(max_plus_one);
+    stopwatch.Stop();
+    Console.WriteLine(sum);
+    Console.WriteLine(stopwatch.ElapsedMilliseconds / 1000.0 + " seconds");
+  }
+
+  private static long SumOfMultiplesBelow(long n, int m1 = 3, int m2 = 5)
+  {
+    long sum = 0;
     for (int i = 1; i < n; i++)
     {
       if (i % m1 == 0 || i % m2 == 0)
@@ -15,6 +29,6 @@ class Problem1
         sum += i;
       }
     }
-    Console.WriteLine(sum);
+    return sum;
   }
 }
