6db022f72cbe8b19219eb5bff0bfd70b97a37925
Kyle Dornblaser <kdornblaser@gmail.com>
2015-06-23 13:23:21 -0400

    Update problem1.rb with elapsed time.

--- Ruby/problem1.rb
+++ Ruby/problem1.rb
@@ -8,4 +8,6 @@ def sum_of_multiples_below(n, m1 = 3, m2 = 5)
 end
 
 max_plus_one = ARGV.first ? ARGV.first.to_i : 10
+start_time = Time.now
 puts sum_of_multiples_below(max_plus_one)
+puts "#{Time.now - start_time} seconds."
