e36b1b99befc4bb62d16b4dc1a92e83418e04e24
Kyle Dornblaser <kdornblaser@gmail.com>
2015-06-25 12:27:24 -0400

    Add time elapsed.

--- Ruby/problem5.rb
+++ Ruby/problem5.rb
@@ -7,4 +7,6 @@ def smallest_multiple(min, max)
 end
 
 min, max = ARGV[1] ? ARGV[0..1].map {|v| v.to_i } : [1,10]
+start_time = Time.now
 puts smallest_multiple(min, max)
+puts "#{Time.now - start_time} seconds."
